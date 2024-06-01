6c8d816492786fbc900f95b582135b9868d14e18
DyllanRuiz <dyllan.ruiz.vasquez@est.una.ac.cr>
2024-06-01 12:56:43 -0600

    chore: Add new allowed origin for the online store front-end

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,7 +5,8 @@
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://tienda-online-front-exam-9j586zg6l-dyllan-ruizs-projects.vercel.app/"
+    "https://tienda-online-front-exam-9j586zg6l-dyllan-ruizs-projects.vercel.app/",
+    "https://tienda-online-front-exam.vercel.app/"
 ];
 
 export default allowedOrigins;
